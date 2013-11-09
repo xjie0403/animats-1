4392b65baa1f273c784568f1c62a7b6a9fa314ab
Ross Hansen <hansenrl@outlook.com>
2013-11-09 14:57:01 -0800

    changed how food and predators are stored, added better interface to the brain of the animat

--- Animat.py
+++ Animat.py
@@ -10,7 +10,6 @@ class Animat:
 
     def train(self, animatInputs, animatOutputs):
         """
-
         @param animatInputs: a list of AnimatInput objects to train on
         @param animatOutputs: a list of AnimatOutput objects to train on
         """
@@ -21,6 +20,8 @@ class Animat:
                                      [output.openMouth, output.hide])
             self.brain.trainVocal([input.fed, input.hurt],
                                   [output.make1, output.make2])
+            self.openMouth = 0
+            self.hide = 0
 
     def getTrainingData(self):
         dataInputs = [(0,0),(1,0),(0,1),(1,1)]
@@ -34,6 +35,24 @@ class Animat:
                 outputs.append(output)
 
         return [inputs, outputs]
+
+    def mouthOpen(self):
+        return self.openMouth
+
+    def hide(self):
+        return self.hide
+
+    def timeCyle(self, hear1, hear2, fed, hurt):
+        """
+
+        @param inputs: a list of 4 floats (hear1, hear2, fed, hurt)
+        @return: nothing #a list of 4 integers (openMouth, hide, make1, make2)
+        """
+        [openMouth, hide, make1, make2] = self.brain.activateNetworks([hear1, hear2, fed, hurt])
+        self.openMouth = openMouth
+        self.hide = hide
+
+
 
 #a = Animat()
 #a.getTrainingData()
--- Environment.py
+++ Environment.py
@@ -7,6 +7,13 @@ class XYValues:
     
     xValue = 0
     yValue = 0
+    def __init__(self):
+        self.xValue = 0
+        self.yValue = 0
+
+    def __init__(self, x, y):
+        self.xValue = x
+        self.yValue = y
 
     def setX(self, x):
         self.xValue = x
@@ -26,18 +33,27 @@ class XYValues:
 
 
 class Environment:
+    #environmentSize = 1000
+    #animats = [[Animat() for i in range(environmentSize)] for j in range(environmentSize)]
 
-    animats = [[Animat() for i in range(1000)] for j in range(1000)]
+    #food = [XYValues() for k in range(100)]
 
-    food = [XYValues() for k in range(100)]
+    #predators = [XYValues() for l in range(100)]
 
-    predators = [XYValues() for l in range(100)]
+    def __init__(self):
+        self.environmentSize = 100
+        self.animats = []
+        for i in range(self.environmentSize):
+            self.animats.append([])
+            for j in range(self.environmentSize):
+                self.animats[i].append([Animat(), 0, 0])
 
+    """
     def generateRandomFood(self):
 
         for i in self.food:
-            i.setX(random.randint(0, 1000))
-            i.setY(random.randint(0, 1000))
+            i.setX(random.randint(0, self.environmentSize))
+            i.setY(random.randint(0, self.environmentSize))
 
         print "Here are the coordinates for the food:"
         for j in self.food:
@@ -47,8 +63,8 @@ class Environment:
     def generateRandomPredators(self):
 
         for i in self.predators:
-            i.setX(random.randint(0, 1000))
-            i.setY(random.randint(0, 1000))
+            i.setX(random.randint(0, self.environmentSize))
+            i.setY(random.randint(0, self.environmentSize))
 
         print "Here are the coordinates for the predators:"
         for j in self.predators:
@@ -73,10 +89,23 @@ class Environment:
         #print "Here are the predator coordinates after moving:"
         #for j in self.predators:
         #    print j.getCoordinates()
-
+    """
+    def timeCycle(self):
+        for i in range(self.environmentSize):
+            for j in range(self.environmentSize):
+                tile = self.animats[i,j]
+                if tile[1] == 1 and tile[0].mouthOpen():
+                    fed = 1
+                else :
+                    fed = 0
+                if tile[2] == 1 and (not tile[0].hide()):
+                    hurt = 1
+                else:
+                    hurt = 0
+                tile[0].timeCyle(0,0,fed,hurt)
 
 environment = Environment()
-environment.generateRandomFood()
-environment.generateRandomPredators()
-environment.moveFood()
-environment.movePredators()
+#environment.generateRandomFood()
+#environment.generateRandomPredators()
+#environment.moveFood()
+#environment.movePredators()
