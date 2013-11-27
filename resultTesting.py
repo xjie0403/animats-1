606e8bcee6dde108f663dcfa4ffee79e5358d115
Ross Hansen <hansenrl@outlook.com>
2013-11-26 19:39:07 -0800

    removed hidden layers from Brain

--- resultTesting.py
+++ resultTesting.py
@@ -1,9 +1,9 @@
 from Environment import *
 
 environment = Environment()
-environment.trainPerfectBlock(4,5,4,5)
+environment.trainPerfectBlock(5,5,5,5)
 testN = 5
-for century in range(100):
+for century in range(300):
     print "Beginning century {0}".format(century)
     for round in range(100):
         environment.timeCycle()
