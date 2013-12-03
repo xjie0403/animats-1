a6b583faba29eaa73c0c17ea931f5659fefd11d8
Roshan Doolabh <roshan.doolabh@wizardsofgyan.com>
2013-12-03 01:09:05 -0800

    added CSV export to master

--- resultTesting.py
+++ resultTesting.py
@@ -1,10 +1,14 @@
 from Environment import *
+import csv
 
 environment = Environment()
 environment.trainPerfectBlock(6,10,6,10,1)
 environment.trainPerfectBlock(26,30,26,30,2)
 testN = 9
-for century in range(500):
+cntArray = []
+numCenturies = 400
+
+for century in range(numCenturies):
     print "Beginning century {0}".format(century)
     for round in range(100):
         environment.timeCycle()
@@ -17,6 +21,9 @@ for century in range(500):
     environment.trainCycle()
     cnt = environment.getBehaviors()
 
+    #each count object is added to an array
+    cntArray.append(cnt)
+
     print environment.animats[testN][testN][0].getSummaryString() + "\t\t" + environment.animats[testN+1][testN][0].getSummaryString()
     if (environment.getHealthiestNeighbor(testN,testN)):
         print "(10,10) neighbor: " + environment.getHealthiestNeighbor(testN,testN).getSummaryString()
@@ -27,3 +34,38 @@ for century in range(500):
     print cnt.most_common(10)
     print cnt['00011011 00011011']
     print cnt['00100111 00100111']
+
+def createCSV():
+    lastCenturyBehaviors = cntArray[numCenturies-1]
+
+    behaviors = []
+
+    for i in list(lastCenturyBehaviors):
+        behaviors.append(i)
+
+    results = [[] for i in range(len(behaviors))]
+
+    resultsCounter = 0
+    for i in cntArray:
+        tempListOfBehaviors = list(i)
+        behaviorCounter = 0
+        for j in behaviors:
+            if j in tempListOfBehaviors:
+                results[behaviorCounter].append(i[j])
+            else:
+                results[behaviorCounter].append(0)
+            behaviorCounter = behaviorCounter +1
+
+    csvMatrix = zip(*results)
+
+    with open('results.csv','wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(behaviors)
+            count = 0
+            for i in csvMatrix:
+                writer.writerow(csvMatrix[count])
+                count = count + 1
+    #temporarily return list of behaviors for image
+    #return behaviors
+
+createCSV()
