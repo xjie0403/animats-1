69ed8d61feee04828567b111040fd40f014f9499
Roshan Doolabh <roshan.doolabh@wizardsofgyan.com>
2013-12-04 22:10:38 -0800

    fixed problems with images and output the key in a txt file

--- CreateImage.py
+++ CreateImage.py
@@ -9,10 +9,14 @@ class CreateImage:
 
     def createBMP(self, fileName, animats, behaviors):
 
-        newBehaviors = self.setBestBehaviors(behaviors)
-        #newBehaviors = behaviors
+        (newBehaviors, bestBehavior1, bestBehavior1RGB, bestBehavior2, bestBehavior2RGB) = self.setBestBehaviors(behaviors)
+
+        self.createKey(bestBehavior1, bestBehavior1RGB, bestBehavior2, bestBehavior2RGB)
 
         self.createBehaviorDictionary(newBehaviors)
+
+        #print "here's what's in the dict: "
+        #print self.behaviorDict
 
         #create a white image
         img = Image.new('RGB', (len(animats), len(animats)), "white")
@@ -32,27 +36,35 @@ class CreateImage:
 
     def setBestBehaviors(self, behaviors):
 
-        if self.perfectStrategy2 in behaviors:
+        bestBehavior1 = ''
+        bestBehavior2 = ''
+        bestBehavior1RGB = 0
+        bestBehavior2RGB = 0
 
+        if self.perfectStrategy2 in behaviors:
             temp = behaviors[0]
             behaviors.pop(behaviors.index(self.perfectStrategy2))
             behaviors[0] = self.perfectStrategy2
             behaviors.append(temp)
+            bestBehavior1 = behaviors[0]
+            bestBehavior1RGB = 0
 
         if self.perfectStrategy1 in behaviors:
             behaviors.append(behaviors.pop(behaviors.index(self.perfectStrategy1)))
+            bestBehavior2 = behaviors[len(behaviors)-1]
+            bestBehavior2RGB = 256
 
-        return behaviors
+        return (behaviors, bestBehavior1, bestBehavior1RGB, bestBehavior2, bestBehavior2RGB)
 
 
     def createBehaviorDictionary(self, behaviors):
 
-        #commented code to set the color variation based on the length
-        #colorVariation = 255/(len(behaviors)-1)
-        #if colorVariation == 0:
-        #    colorVariation = 1
+        colorVariation = 256/(len(behaviors)-1)
 
-        colorVariation = 1
+        #print "HERE IS THE LEN OF BEHAVIORS: " + str(len(behaviors))
+        #print "HERE IS THE COLOR VARIATION NUMBER: " + str(colorVariation)
+        if colorVariation == 0:
+            colorVariation = 1
 
         print "color variation: " + str(colorVariation)
 
@@ -66,7 +78,20 @@ class CreateImage:
                 self.behaviorDict[behavior] = counter
             counter = counter + colorVariation
 
-    def paintPixel(self, behaviorString, x, y, pixels):
-        pixels[x,y] = (self.behaviorDict[behaviorString], self.behaviorDict[behaviorString], self.behaviorDict[behaviorString])
-        #print "printing pixel with RGB: " + str(self.behaviorDict[behaviorString])
+        if self.behaviorDict[self.perfectStrategy1] < 256:
+            self.behaviorDict[self.perfectStrategy1] = 256
+        if self.behaviorDict[self.perfectStrategy2] > 0:
+            self.behaviorDict[self.perfectStrategy2] = 0
 
+
+    def paintPixel(self, behaviorString, x, y, pixels):
+        if behaviorString in self.behaviorDict:
+            pixels[x,y] = (self.behaviorDict[behaviorString], self.behaviorDict[behaviorString], self.behaviorDict[behaviorString])
+        else:
+            pixels[x,y] = (128,128,128)
+
+    def createKey(self, bestBehavior1, bestBehavior1RGB, bestBehavior2, bestBehavior2RGB):
+        output = open("key.txt", "w")
+        output.write("The behavior: " + bestBehavior1 + " has an RGB of: " + str(bestBehavior1RGB) + " - black\n")
+        output.write("The behavior: " + bestBehavior2 + " has an RGB of: " + str(bestBehavior2RGB) + " - white")
+        output.close()
