3c478fb999d90aa837c7ec143c91361a71eda61f
Roshan Doolabh <roshan.doolabh@wizardsofgyan.com>
2013-12-04 23:04:54 -0800

    created csv file for scare

--- resultTesting.py
+++ resultTesting.py
@@ -1,9 +1,13 @@
 from Environment import *
+import csv
 
 environment = Environment()
 environment.trainPerfectBlock(10,12,10,12)
 testN = 5
-for century in range(300):
+cntArray = []
+numCenturies = 200
+
+for century in range(numCenturies):
     print "Beginning century {0}".format(century)
 
     cnt = environment.getBehaviors()
@@ -22,6 +26,9 @@ for century in range(300):
     environment.trainCycle()
     cnt = environment.getBehaviors()
 
+    #each count object is added to an array
+    cntArray.append(cnt)
+
     print environment.animats[testN][testN][0].getSummaryString() + "\t\t" + environment.animats[testN+1][testN][0].getSummaryString()
     if (environment.getHealthiestNeighbor(testN,testN)):
         print "(10,10) neighbor: " + environment.getHealthiestNeighbor(testN,testN).getSummaryString()
@@ -29,3 +36,35 @@ for century in range(300):
         print "(11,10) neighbor: " + environment.getHealthiestNeighbor(testN+1,testN).getSummaryString()
     #print environment.getHealthiestNeighbor(10,10).getSummaryString() + "\t\t" + environment.getHealthiestNeighbor(11,10).getSummaryString() + "\n"
 
+def createCSV():
+    lastCenturyBehaviors = cntArray[numCenturies-1].most_common(10)
+
+    behaviors = []
+
+    for i in lastCenturyBehaviors:
+        behaviors.append(i[0])
+
+    results = [[] for i in range(len(behaviors))]
+
+    resultsCounter = 0
+    for i in cntArray:
+        tempListOfBehaviors = list(i)
+        behaviorCounter = 0
+        for j in behaviors:
+            if j in tempListOfBehaviors:
+                results[behaviorCounter].append(i[j])
+            else:
+                results[behaviorCounter].append(0)
+            behaviorCounter = behaviorCounter +1
+
+    csvMatrix = zip(*results)
+
+    with open('results.csv','wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(behaviors)
+            count = 0
+            for i in csvMatrix:
+                writer.writerow(csvMatrix[count])
+                count = count + 1
+
+createCSV()
