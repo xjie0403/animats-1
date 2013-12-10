f98c28d803a3dfcd462d251a4f1bd055dcfcc2c1
Roshan Doolabh <roshan.doolabh@wizardsofgyan.com>
2013-12-10 09:32:59 -0800

    added CSV to Scare2 branch

--- resultTesting.py
+++ resultTesting.py
@@ -1,9 +1,13 @@
 from Environment import *
+import csv
 
 environment = Environment()
 #environment.trainPerfectBlock(11,12,11,12)
 testN = 11
-for century in range(500):
+cntArray = []
+numCenturies = 500
+
+for century in range(numCenturies):
     print "Beginning century {0}".format(century)
     for round in range(100):
         environment.timeCycle()
@@ -15,6 +19,9 @@ for century in range(500):
     #print environment.getHealthiestNeighbor(10,10).getSummaryString() + "\t\t" + environment.getHealthiestNeighbor(11,10).getSummaryString() + "\n"
     environment.trainCycle()
     cnt = environment.getBehaviors()
+
+    #each count object is added to an array
+    cntArray.append(cnt)
 
     print environment.animats[testN][testN][0].getSummaryString() + "\t\t" + environment.animats[testN+1][testN][0].getSummaryString()
     if (environment.getHealthiestNeighbor(testN,testN)):
@@ -28,3 +35,36 @@ for century in range(500):
 
     #print cnt['00011011 0011']
     #print cnt['01 01']
+
+
+def createCSV():
+    lastCenturyBehaviors = cntArray[numCenturies-1]
+    behaviors = []
+
+    for i in list(lastCenturyBehaviors):
+        behaviors.append(i)
+
+    results = [[] for i in range(len(behaviors))]
+
+    resultsCounter = 0
+    for i in cntArray:
+        tempListOfBehaviors = list(i)
+        behaviorCounter = 0
+        for j in behaviors:
+            if j in tempListOfBehaviors:
+                results[behaviorCounter].append(i[j])
+            else:
+                results[behaviorCounter].append(0)
+            behaviorCounter = behaviorCounter +1
+
+    csvMatrix = zip(*results)
+
+    with open('results.csv','wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(behaviors)
+            count = 0
+            for i in csvMatrix:
+                writer.writerow(csvMatrix[count])
+                count = count + 1
+
+createCSV()
